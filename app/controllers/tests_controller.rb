dc3a058f4d43317ee44625f66bd933f6d1a56b71
Anna1327 <aoi.yatsu@ya.ru>
2022-02-11 02:17:59 +0300

    lesson11

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,51 +3,12 @@
 class TestsController < ApplicationController
 
   before_action :authenticate_user!  
-  before_action :find_test, only: %i[show edit update destroy start]
-  after_action :send_log_message
-  around_action :log_execute_time
+  before_action :find_test, only: %i[start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
     @tests = Test.all
-  end
-
-  def show
-    render inline: '<%= @test.inspect %>'
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.new(tests_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def search
-  end
-
-  def edit
-  end
-
-  def update
-    if @test.update(tests_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -57,23 +18,8 @@ class TestsController < ApplicationController
 
   private
 
-  def tests_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
-  end
-
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def send_log_message
-    logger.info("Action #{action_name} was finished")
-  end
-
-  def log_execute_time
-    start = Time.now
-    yield
-    finish = Time.now - start
-    logger.info("Execution time: #{finish * 1000} ms")
   end
 
   def rescue_with_test_not_found
--- app/helpers/sessions_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module SessionsHelper
-  def flash_alert
-    if flash[:alert] 
-        content_tag :p, flash[:alert], class: 'flash alert'
-    end
-  end
-end
